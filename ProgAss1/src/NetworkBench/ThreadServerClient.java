2c7ea4c3656cce0cf9b6e10f5b8c68335244fdec
KamalNath_NG <KamalNath_NG@BLRKEC48322L.ad.infosys.com>
2013-01-29 16:35:23 +0530

    Networking threaded client

---
+++ ProgAss1/src/NetworkBench/ThreadServerClient.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NetworkBench;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ *
+ * @author kamalnath_ng
+ */
+class ThreadServerClient extends Thread {
+    Socket clientSocket = null;
+    ThreadServerClient(Socket clientSocket) {
+        this.clientSocket =clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            PrintStream os  ;
+            
+            /*
+             * Create input and output streams for this client.
+             */
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            os = new PrintStream(clientSocket.getOutputStream());
+
+            /*
+             * Start the conversation.
+             */
+            while (true) {
+                String line = reader.readLine();
+                if (line.startsWith("/quit")) {
+                    break;
+                }
+                os.println("*** from server " + line + " ***");
+            }
+            /*
+             * Close the output stream, close the input stream, close the
+             * socket.
+             */
+
+            os.close();
+            reader.close();
+            clientSocket.close();
+        } catch (IOException e) {
+        }
+    }
+}
